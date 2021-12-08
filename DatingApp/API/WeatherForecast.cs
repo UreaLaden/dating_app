cd4412a8050f25675ed1dc8a1736b95b893dd74e
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-12-08 07:50:22 -0500

    Completed walking skeleton

--- DatingApp/API/WeatherForecast.cs
+++ DatingApp/API/WeatherForecast.cs
@@ -1,6 +1,10 @@
-namespace API;
+using System;
 
+namespace API
+{
+    
 public class WeatherForecast
+
 {
     public DateTime Date { get; set; }
 
@@ -9,4 +13,5 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string Summary { get; set; }
+}
 }
