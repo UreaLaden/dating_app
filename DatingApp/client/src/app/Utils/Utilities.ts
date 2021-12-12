4b7f2590e4c8db1b6bdeff087a6df1b2a2615773
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-12-12 09:37:15 -0500

    Added bootswatch a connected registration/login

---
+++ DatingApp/client/src/app/Utils/Utilities.ts
@@ -0,0 +1,6 @@
+
+export const Capitalize = (stringToCapitalize:string) => {
+    let firstLetter:string = stringToCapitalize.charAt(0).toUpperCase();
+    let remainingChars:string = stringToCapitalize.slice(1);
+      return firstLetter + remainingChars;
+}
--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Capitalize } from '../Utils/Utilities';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -9,19 +12,24 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(
+    public accountService: AccountService,
+    private router: Router, 
+    private toastr:ToastrService) { }
 
   ngOnInit(): void {
   }
   login(){
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
     }, error =>{
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
