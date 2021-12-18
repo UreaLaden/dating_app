98bc0b0fadcf0adeac67cf5b17952b0be3b8c71f
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-12-18 11:17:40 -0500

    added photo upload functionality

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -40,13 +40,15 @@ namespace API.Controllers
 
             return new UserDto{
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
             };
         }
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Register(LoginDto loginDto){
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users
+                .Include(p =>p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if(user == null) return Unauthorized("Invalid username");
 
@@ -60,7 +62,8 @@ namespace API.Controllers
 
             return new UserDto{
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
